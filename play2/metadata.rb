3e25c8b62e2075c45a5aa8c213bab090d34b86a4
somnathmgit <somnathsidea@gmail.com>
2014-10-23 10:59:26 +0530

    ok

--- play2/metadata.rb
+++ play2/metadata.rb
@@ -4,11 +4,3 @@ maintainer_email "somnathsidea@gmail.com"
 license "google"
 description "Installs/Configures Play2 Framework (Version 2.1.4)"
 version "1.0"
-
-%w{ s3_file }.each do |cookbook|
-  depends cookbook
-end
-
-%w{ centos redhat fedora amazon}.each do |os|
-  supports os
-end
