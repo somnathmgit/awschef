bca09e8677b890de2b2242deb0a66073cecf48d4
somnathmgit <somnathsidea@gmail.com>
2014-10-23 12:32:18 +0530

    ik

--- play2/recipes/default.rb
+++ play2/recipes/default.rb
@@ -1,5 +1,5 @@
 # create a file in ec2 instance
 execute "unzip play" do
-    command "touch /home/ec2-user/test.txt"
+    command "echo #{node['aws-access']['aws_access_key_id']} >> /home/ec2-user/test.txt"
 end	
 
