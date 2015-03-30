b5139650a6ee36dc8031029b08f8d25ff443b1d1
somnathmgit <somnathmgit@users.noreply.github.com>
2015-03-30 15:51:21 +0530

    updated the cookbook to apache tomcat webserver

--- play2/recipes/default.rb
+++ play2/recipes/default.rb
@@ -1,5 +1,13 @@
 # create a file in ec2 instance
-execute "unzip play" do
-    command "echo '#{node['aws-access']['aws_access_key_id']}' >> /home/ec2-user/test.txt"
-end	
+#execute "unzip play" do
+#    command "echo '#{node['aws-access']['aws_access_key_id']}' >> /home/ec2-user/test.txt"
+#end	
 
+package "apache" do
+   action :install
+end
+
+service "apache" do
+  supports :restart => true, :reload => true
+  action :enable
+end
