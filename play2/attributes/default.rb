466d395266979b02fcd7b985a5ee4f1e3a91f571
somnathmgit <somnathmgit@users.noreply.github.com>
2015-03-30 16:55:14 +0530

    apache version added.

--- play2/attributes/default.rb
+++ play2/attributes/default.rb
@@ -3,14 +3,4 @@
 # Cookbook Name:: Play2
 # Attributes:: default
 
-default['play']['base_version']="2"
-default['play']['full_version']="2.1.1"
-default['play']['installer']['file']="play-#{node['play']['full_version']}.zip"
-default['play']['installer']['s3-bucket-name'] = "applications-repo"
-default['play']['application']['location'] = "/usr/share"
-default['play']['application']['target-path'] = "#{node['play']['application']['location']}/play-#{node['play']['full_version']}/play"
-default['play']['application']['link-name'] = "/usr/bin/play#{node['play']['base_version']}"
-
-
-default['aws-access']['aws_access_key_id'] = "<will-take-the-value-from-custom-JSON>"
-default['aws-access']['aws_secret_access_key'] = "<will-take-the-value-from-custom-JSON>"
+default['apache']['version']="2.2"
