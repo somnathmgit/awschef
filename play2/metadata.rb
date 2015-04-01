d9bc15f5aad3d4b71993fa8efab7c74e93758f8d
somnathmgit <somnathmgit@users.noreply.github.com>
2015-04-01 14:51:45 +0530

    updated

--- play2/metadata.rb
+++ play2/metadata.rb
@@ -1,7 +1,7 @@
 # This is play2 metadata
 name "play2"
-maintainer "somnath"
-maintainer_email "somnathsidea@gmail.com"
-license "google"
-description "Installs/Configures Play2 Framework (Version 2.1.4)"
+#maintainer "name"
+#maintainer_email "email-id"
+#license "google"
+#description "Installs/Configures apache"
 version "1.0"
