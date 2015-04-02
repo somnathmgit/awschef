bd544d40d17e84e239d9ca756a87fd217e84512a
somnathmgit <somnathmgit@users.noreply.github.com>
2015-04-02 13:17:19 +0530

    comment removed

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -1,8 +1,3 @@
-# create a file in ec2 instance
-#execute "unzip play" do
-#    command "echo '#{node['aws-access']['aws_access_key_id']}' >> /home/ec2-user/test.txt"
-#end	
-
 package 'httpd' do
   action :install
 end
