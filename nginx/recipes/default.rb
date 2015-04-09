0ec288555ca5abc8b7fc2105e7e4e8691928e031
somnathmgit <somnathmgit@users.noreply.github.com>
2015-04-09 16:40:52 +0530

    updated

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -6,10 +6,10 @@ service 'nginx' do
   action [:enable, :start]
 end
 
-#file "/var/www/html/#{node['index_page']['name']}" do
-#  content "<html>
-#  <body>
-#    <h1>#{node['welcome']['message']}</h1>
-#  </body>
-#</html>"
-#end
+file "/var/www/html/#{node['index_page']['name']}" do
+  content "<html>
+  <body>
+    <h1>#{node['welcome']['message']}</h1>
+  </body>
+</html>"
+end
