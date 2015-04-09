2d964e52a6d9af524f4feabe510d42404560a31d
somnathmgit <somnathmgit@users.noreply.github.com>
2015-04-09 16:15:46 +0530

    content added

---
+++ nginx/recipes/default.rb
@@ -0,0 +1,15 @@
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [:enable, :start]
+end
+
+#file "/var/www/html/#{node['index_page']['name']}" do
+#  content "<html>
+#  <body>
+#    <h1>#{node['welcome']['message']}</h1>
+#  </body>
+#</html>"
+#end
