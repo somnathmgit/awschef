6d79dacbd1c972fecaf3ca83f08ff9ae2b1e078c
somnathmgit <somnathmgit@users.noreply.github.com>
2015-06-17 13:09:36 +0530

    ok

--- hostname/recipes/default.rb
+++ hostname/recipes/default.rb
@@ -1,15 +1,19 @@
-package 'httpd' do
-  action :install
-end
-
-service 'httpd' do
-  action [:enable, :start]
-end
-
-file "/var/www/html/#{node['index_page']['name']}" do
-  content "<html>
-  <body>
-    <h1>#{node['welcome']['message']}</h1>
-  </body>
-</html>"
-end
+if node['ec2'].nill?
+  bash "set_hostname" do
+   #interpreter "bash"
+   code <<-EOH
+   NODE_NAME=node['hostname']
+   LOCAL_IP=`ifconfig eth0 |grep inet |awk '{print $2}' |cut -d":" -f2 |grep -v "^$"`
+   hostname ${NODE_NAME}
+   echo ${LOCAL_IP}  ${NODE_NAME} >> /etc/hosts
+   EOH 
+else
+   bash "set_hostname" do
+   #interpreter "bash"
+   code <<-EOH
+   NODE_NAME=$(curl -s http://169.254.169.254/latest/meta-data/instance-id | cut -d- -f2)
+   LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+   hostname ${NODE_NAME}
+   echo ${LOCAL_IP}  ${NODE_NAME} >> /etc/hosts
+   EOH
+end	
