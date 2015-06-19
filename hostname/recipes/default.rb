8b253aa1b46b7881da230a670639d2f1514ff611
somnathmgit <somnathmgit@users.noreply.github.com>
2015-06-19 14:29:45 +0530

    Update default.rb

--- hostname/recipes/default.rb
+++ hostname/recipes/default.rb
@@ -1,5 +1,15 @@
-execute "deploy-war" do  
+execute "deploy-war-with-execute" do  
   cwd "/home/ec2-user"
   command "wget https://web-actions.googlecode.com/files/helloworld.war"
-  action :run
+  action :nothing
+end
+
+bash 'deploy-war-with-bash' do
+  user 'root'
+  cwd '/home/ec2-user'
+  code <<-EOH
+	  mkdir webapps
+	  wget wget https://web-actions.googlecode.com/files/helloworld.war
+	  cp helloworld.war webapps/
+  EOH
 end
